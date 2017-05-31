90bcafa1c38c266076cb3d5c1729129f05994dfc
Adam Kwan <adamkwan@Adams-MacBook.home>
2017-05-31 16:25:12 -0400

    loop for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ numberRight = 0;
 
 var questions = [ {
 	question: "Who killed Eddard Stark?",
-	chocies: ["The Mountain", "Ser Ilyn Payne", "The Hound", "Little Finger"],
+	choices: ["The Mountain", "Ser Ilyn Payne", "The Hound", "Little Finger"],
 	correctAnswer: "Ser Ilyn Payne"
 }, {
 	question: "What was the name of Rob Stark's direwolf?",
@@ -23,13 +23,69 @@ var questions = [ {
 	correctAnswer: "Balerion"
 }];
 
+console.log (questions[1].question);
 
-function grader(){
-if (document.getElementById("opt1").checked === true) {
-	numberRight ++;
-	$("#right").html(numberRight);
-}
-}
+for (var i = 0; i < questions.length; i++) {
+	var pTag = $("<p>");
+	var form = $("<form>");
+
+	//set our question
+	pTag.html(questions[i].question);
+	
+	// console.log(questions[i].choices)
+	// debugger
+	for(var j = 0; j < questions[i].choices.length; j++){
+		var label = $("<label class='radio-inline'>");
+		// label.addClass("radio-inline");
+
+		var choice = $("<input type='radio' name='optradio'>");
+
+		//set attributes for the choice
+		// choice.attr("type","radio");
+		// choice.attr("name","optradio");
+		choice.attr("value", questions[i].choices[j]);
+		label.append(choice);
+		label.append(questions[i].choices[j]);
+		form.append(label);
+		pTag.append(form);
+	}
+
+	$('#questionDiv').append(pTag);
+
+
+			
+} // end for
+
+// "<p>" + question[i].question + "</p>"
+// 		"<form>
+// 			<label class="radio-inline">
+// 				<input value=''  type="radio" name="optradio">The Mountain
+// 			</label>
+// 			<label class="radio-inline">
+// 				<input type="radio" name="optradio">Ser Ilyn Payne
+// 			</label>
+// 			<label class="radio-inline">
+// 				<input type="radio" name="optradio">The Hound
+// 			</label>
+// 			<label class="radio-inline">
+// 				<input type="radio" name="optradio">Little Finger
+// 			</label>
+// 		</form>
+
+
+
+
+
+
+
+// //Grades
+// function grader(){
+// if (document.getElementById("opt1").checked === true) {
+// 	numberRight ++;
+// 	$("#right").html(numberRight);
+// }
+// }
+
 
 //Countdown Timer
 var n = 5;
@@ -44,9 +100,7 @@ function countDown(){
    $("#countdown").html(n);
 
    if (n === 0){
-   grader();	
+   // grader();	
    }
-   
-
 }
 
